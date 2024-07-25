ea373fd0974c122879b3e191cc44cef30592b9c1
metamonk <metamonk@ratlabs.xyz>
2024-07-25 00:25:15 -0500

    feat: add overlay

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col gap-2 max-w-[500px] mx-auto">
         <h1 className="text-2xl font-bold">Bratt Utilities</h1>
-        <p className="text-sm text-gray-500">Be Bratt</p>
+        {/* <p className="text-sm text-gray-500">Be Bratt</p> */}
         <div className="flex flex-col gap-2">
           <Link href="/pilled">
             <Button>Bratt Pilled 🟩</Button>
